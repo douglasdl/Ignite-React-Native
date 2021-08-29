90696d515a04889691d111338482f355b344b4ec
douglasdl <douglas_san@hotmail.com>
2021-08-28 23:47:54 -0300

    Add type to object and state

--- myskills/src/pages/Home.tsx
+++ myskills/src/pages/Home.tsx
@@ -13,13 +13,26 @@ import {
 import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
 
+interface SkillData {
+    id: string;
+    name: string;
+}
+
 export function Home() {
     const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([]);
+    const [mySkills, setMySkills] = useState<SkillData[]>([]);
     const [greeting, setGreeting] = useState('');
 
     function handleAddNewSkill() {
-        setMySkills(oldState => [...oldState, newSkill]);
+
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+
+        console.log(data);
+
+        setMySkills(oldState => [...oldState, data]);
     }
 
     useEffect(() => {
@@ -66,9 +79,9 @@ export function Home() {
 
             <FlatList 
                 data={mySkills}
-                keyExtractor={item => item}
+                keyExtractor={item => item.id}
                 renderItem={({ item }) => (
-                <SkillCard skill={item} />
+                    <SkillCard skill={item.name} />
                 )}
             />
         
